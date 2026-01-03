981d7c9c495f8af996a086b7078ae3bf7fd2b537
Bert Plasschaert <bert.plasschaert@outlook.com>
2026-01-03 21:27:20 +0100

    updated workflow

--- python/taggablebanner/main.py
+++ python/taggablebanner/main.py
@@ -6,7 +6,7 @@ def run_username_check(username: str):
     registered_usernames = markdownmanager.get_names()
 
     if username in registered_usernames:
-        ValueError("username already on homepage")
+        raise ValueError("username already on homepage")
 
 
 def add_username(username: str):

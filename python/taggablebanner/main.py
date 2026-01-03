951433121cb4070e8906393c381ec7b288555e22
Bert Plasschaert <bert.plasschaert@outlook.com>
2026-01-03 22:43:55 +0100

    dissabled the name check

--- python/taggablebanner/main.py
+++ python/taggablebanner/main.py
@@ -6,8 +6,8 @@ def run_username_check(username: str):
     registered_usernames = markdownmanager.get_names()
 
     # TODO: enable this again
-    if username in registered_usernames:
-        raise ValueError("username already on homepage")
+    # if username in registered_usernames:
+    #     raise ValueError("username already on homepage")
 
 
 def add_username(username: str):
